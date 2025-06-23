cacb6124799ffd892b801dc37f3ff11d2634bb8a
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-06-23 20:06:39 -0300

    Update files

---
+++ essentials/basics.js
@@ -0,0 +1,8 @@
+var userName;
+var userAge = 38;
+userName = 'Max';
+function add(a, b) {
+    if (b === void 0) { b = 9; }
+    return a + b;
+}
+add(10);
---
+++ essentials/basics.ts
@@ -0,0 +1,10 @@
+let userName: string;
+let userAge = 38;
+
+userName = 'Max';
+
+function add(a: number, b: number = 9) {
+    return a + b
+}
+
+add(10);
